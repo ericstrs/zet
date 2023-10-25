d946e1058af21e1f5cfd8d193464d5ec6650f666
oneseIf <eric.garcia12317@gmail.com>
2023-10-25 13:32:15 -0500

    doc: add zet command comment

--- cmd/zet/main.go
+++ cmd/zet/main.go
@@ -1,3 +1,19 @@
+/*
+Zet is a command-line utility for managing a Zettelkasten.
+It employs a command-line interface (CLI) for core and batch features and a text-based user interface (TUI) for searching and visualization functionalities.
+
+Usage:
+
+	zet [command] [arguments]
+
+Commands:
+
+	add    - Adds a new zettel with the given title and content.
+	remove - Removes a zettel by its ID.
+	search - Searches for zettels given a query string.
+	merge  - Merges linked notes to form a single note.
+	list   - Lists all existing zettels.
+*/
 package main
 
 func main() {
---
+++ zet.go
@@ -0,0 +1,6 @@
+// Package zet implements basic utilities for managing zettels
+// within the zet system.
+//
+// The zet package contains operations for adding, removing, and
+// merging zettels.
+package zet
