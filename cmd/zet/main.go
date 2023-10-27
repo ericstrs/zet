8a8f916fb229423a66d862ac3626e051fb73330b
oneseIf <eric.garcia12317@gmail.com>
2023-10-26 20:51:38 -0500

    feat: add basic argument handling

--- cmd/zet/main.go
+++ cmd/zet/main.go
@@ -16,5 +16,51 @@ Commands:
 */
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	z "github.com/iuiq/zet"
+	"github.com/iuiq/zet/internal/config"
+)
+
+const usage = `Usage:
+
+  zet [command] [arguments]
+
+Commands:
+
+  add    - Adds a new zettel with the given title and content.
+  remove - Removes a zettel by its ID.
+  search - Searches for zettels given a query string.
+  merge  - Merges linked notes to form a single note.
+  list   - Lists all existing zettels.`
+
 func main() {
+	if err := Run(); err != nil {
+		log.Println(err)
+	}
+}
+
+func Run() error {
+	if len(os.Args) == 1 {
+		return fmt.Errorf("Not enough arguments.\n%s", usage)
+	}
+
+	switch strings.ToLower(os.Args[1]) {
+	case "add": // add a new zettel
+		c := new(config.C)
+		if err := c.Init(); err != nil {
+			return err
+		}
+		if err := z.Add(c.ZetDir); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("Invalid argument.\n%s", usage)
+	}
+
+	return nil
 }
