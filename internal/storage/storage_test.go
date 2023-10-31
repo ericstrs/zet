2260ba239a8c03834dd4dcfe3a9a43db6ad5dd83
oneseIf <eric.garcia12317@gmail.com>
2023-10-30 20:43:33 -0500

    test: add test for process file func

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -25,7 +26,7 @@ func getDBConnection() (*sqlx.DB, error) {
 func getTestTransaction() (*sqlx.Tx, error) {
 	// Connect to the test database
 	// Use an in-memory SQLite database for tests
-	db, err := sqlx.Connect("sqlite3", ":memory:")
+	db, err := sqlx.Connect("sqlite", ":memory:")
 	if err != nil {
 		return nil, err
 	}
@@ -60,35 +61,40 @@ func setupTestDB(db *sqlx.DB) error {
         );
     `)
 
+	return err
+}
+
+func insertDummyData(db *sqlx.DB) error {
 	// Insert dummy data
-	_, err = db.Exec(`
-				-- Create dummy data for directories table
-				INSERT INTO dirs(name) VALUES('20231028012959');
-				INSERT INTO dirs(name) VALUES('20231028013010');
-				INSERT INTO dirs(name) VALUES('20231028013031');
-				INSERT INTO dirs(name) VALUES('20231028013045');
-				INSERT INTO dirs(name) VALUES('20231028013100');
+	_, err := db.Exec(`
+        -- Create dummy data for directories table
+        INSERT INTO dirs(name) VALUES('20231028012959');
+        INSERT INTO dirs(name) VALUES('20231028013010');
+        INSERT INTO dirs(name) VALUES('20231028013031');
+        INSERT INTO dirs(name) VALUES('20231028013045');
+        INSERT INTO dirs(name) VALUES('20231028013100');
 
-				-- Create dummy data for Files table
-				-- For directory 20231028012959
-				INSERT INTO Files(name, content, mtime, dir_name) VALUES('README.md', '# README', '2023-10-28T01:29:59Z', '20231028012959');
+        -- Create dummy data for Files table
+        -- For directory 20231028012959
+        INSERT INTO Files(name, content, mtime, dir_name) VALUES('README.md', '# README', '2023-  10-28T01:29:59Z', '20231028012959');
 
-				-- For directory 20231028013010
-				INSERT INTO Files(name, content, mtime, dir_name) VALUES('README.md', '# README', '2023-10-28T01:30:10Z', '20231028013010');
+        -- For directory 20231028013010
+        INSERT INTO Files(name, content, mtime, dir_name) VALUES('README.md', '# README', '2023-  10-28T01:30:10Z', '20231028013010');
 
-				-- For directory 20231028013031
-				INSERT INTO Files(name, content, mtime, dir_name) VALUES('README.md', '# README', '2023-10-28T01:30:31Z', '20231028013031');
-				INSERT INTO Files(name, content, mtime, dir_name) VALUES('outline.md', '# Outline', '2023-10-28T01:30:31Z', '20231028013031');
+        -- For directory 20231028013031
+        INSERT INTO Files(name, content, mtime, dir_name) VALUES('README.md', '# README', '2023-  10-28T01:30:31Z', '20231028013031');
+        INSERT INTO Files(name, content, mtime, dir_name) VALUES('outline.md', '# Outline', '2023-10-28T01:30:31Z', '20231028013031');
 
-				-- For directory 20231028013045
-				INSERT INTO Files(name, content, mtime, dir_name) VALUES('README.md', '# README', '2023-10-28T01:30:45Z', '20231028013045');
-				INSERT INTO Files(name, content, mtime, dir_name) VALUES('outline.md', '# Outline', '2023-10-28T01:30:45Z', '20231028013045');
+        -- For directory 20231028013045
+        INSERT INTO Files(name, content, mtime, dir_name) VALUES('README.md', '# README', '2023-  10-28T01:30:45Z', '20231028013045');
+        INSERT INTO Files(name, content, mtime, dir_name) VALUES('outline.md', '# Outline', '2023-10-28T01:30:45Z', '20231028013045');
 
     `)
 	if err != nil {
 		return err
 	}
-	return err
+
+	return nil
 }
 
 func ExampleGetExistingZettels() {
@@ -98,6 +104,11 @@ func ExampleGetExistingZettels() {
 		return
 	}
 	defer db.Close()
+
+	if err := insertDummyData(db); err != nil {
+		fmt.Printf("Failed to insert dummy data: %v\n", err)
+		return
+	}
 
 	ez, err := getExistingZettels(db)
 	if err != nil {
@@ -123,4 +134,35 @@ func ExampleGetExistingZettels() {
 	// ./20231028013031/outline.md id: 4
 	// ./20231028013045/README.md id: 5
 	// ./20231028013045/outline.md id: 6
+}
+
+func ExampleProcessFile() {
+	tx, err := getTestTransaction()
+	if err != nil {
+		fmt.Printf("Failed to establish database connection: %v.\n", err)
+		return
+	}
+	defer tx.Rollback()
+
+	testZetDir := filepath.Join("..", "testdata", "zet")
+
+	if err := processZettels(tx, testZetDir, make(map[string]map[string]file)); err != nil {
+		fmt.Printf("Failed to process zettels: %v\n", err)
+		return
+	}
+
+	// Validate zettel insertions.
+	files := []file{}
+	err = tx.Select(&files, "SELECT * FROM files;")
+	if err != nil {
+		fmt.Printf("Failed to select database zettels: %v\n", err)
+		return
+	}
+
+	for _, f := range files {
+		fmt.Printf("./%s/%s id: %d\n", f.DirName, f.Name, f.Id)
+	}
+
+	// Output:
+	// then wot
 }
