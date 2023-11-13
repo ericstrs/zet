872197c30865a9e80c4a125e74bc6127d0973923
oneseIf <eric.garcia12317@gmail.com>
2023-11-13 14:05:12 -0600

    feat(search): add support for direct query and stdout results
    Implement functionality in the CLI to allow users to input a search
    query directly and receive formatted search results in stdout. This
    feature enables non-interactive use of the application, making it
    suitable for scripting and automation. It parses the user-provided
    query, performs the search, and outputs the results in a human-readable
    format, consistent with the interactive mode.

--- internal/ui/cli.go
+++ internal/ui/cli.go
@@ -8,15 +8,20 @@ import (
 	"github.com/iuiq/zet/internal/storage"
 )
 
+const (
+	yellow = "\033[33m" // ANSI escape code for yellow
+	red    = "\033[31m" // ANSI escape code for red
+	reset  = "\033[0m"  // ANSI escape code to reset to default color
+)
+
 var searchUsage = `NAME
 
   search - searches for zettels.
 
 USAGE
 
-  zet search [term]          - Print zettels given a simple search term.
-  zet search browse|b        - Interactively search for a zettel.
-  zet search history [clear] - Print search history or optionally delete it.
+  zet search query|q [term]  - Print zettels given a search term.
+  zet search browse|b [term] - Interactively search for a zettel.
   zet search help            - Print zettels given a search term.`
 
 func SearchCmd(args []string) error {
@@ -30,16 +35,44 @@ func SearchCmd(args []string) error {
 	}
 	n := len(args)
 
-	switch n {
-	case 2: // print results to stdout
-	case 3:
+	if n >= 3 {
+		query := strings.Join(args[3:], " ")
 		switch strings.ToLower(args[2]) {
+		case `query`, `q`:
+			if query == "" {
+				return nil
+			}
+			zettels, err := s.SearchZettels(query, red, reset)
+			if err != nil {
+				zettels = []storage.ResultZettel{storage.ResultZettel{TitleSnippet: "Incorrect syntax"}}
+			}
+			for _, z := range zettels {
+				fmt.Println(yellow + z.DirName + reset + " " + z.TitleSnippet)
+				if z.BodySnippet != "" {
+					fmt.Println(removeEmptyLines(z.BodySnippet))
+				}
+				if z.TagsSnippet != "" {
+					hashedTags := "\t\t#" + strings.ReplaceAll(z.TagsSnippet, " ", " #")
+					fmt.Println(hashedTags)
+				}
+			}
 		case `browse`, `b`:
-			if err := NewSearchUI(s, c.ZetDir, c.Editor).Run(); err != nil {
+			if err := NewSearchUI(s, query, c.ZetDir, c.Editor).Run(); err != nil {
 				return fmt.Errorf("Error running search ui: %v", err)
 			}
 		}
 	}
 
 	return nil
+}
+
+func removeEmptyLines(str string) string {
+	lines := strings.Split(str, "\n")
+	var nonEmptyLines []string
+	for _, line := range lines {
+		if strings.TrimSpace(line) != "" {
+			nonEmptyLines = append(nonEmptyLines, line)
+		}
+	}
+	return strings.Join(nonEmptyLines, "\n")
 }
