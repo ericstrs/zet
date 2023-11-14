1e465c770bcd0e5a31872f1dd17fdc528749e8b3
oneseIf <eric.garcia12317@gmail.com>
2023-11-13 18:21:12 -0600

    feat(list): support sorting all zettels
    Add sub-commands to sort by creation date, modification date, zettel
    body lengh, and alphabetically sorted titles.

--- internal/meta/list.go
+++ internal/meta/list.go
@@ -15,11 +15,15 @@ var listUsage = `NAME
 
 USAGE
 
-	zet ls|list          - Prints all zettels to stdout.
-	zet ls|list recent   - Prints all zettels sorted by modification time.
-	zet ls|list length   - Prints all zettels sorted by word count.
-	zet ls|list alpha    - Prints all zettels sorted by alphabetical titles.
-	zet ls|list help     - Provides command information.`
+	zet list|ls          - Prints all zettels to stdout sorted by creation date.
+	zet list|ls recent   - Prints all zettels sorted by modification time.
+	zet list|ls length   - Prints all zettels sorted by word count.
+	zet list|ls alpha    - Prints all zettels by alphabetically sorted titles.
+	zet list|ls help     - Provides command information.
+
+DESCRIPTION
+
+The list command serves as a tool viewing a collection of zettels. This command displays a list of all zettels stored in the system. Its main purpose is to output all zettels in an organized manner, in ascending order.`
 
 // ListCmd parses and validates user arguments for the list command.
 // If arguments are valid, it calls the desired operation.
@@ -32,7 +36,7 @@ func ListCmd(args []string) error {
 
 	switch n {
 	case 2: // no args
-		l, err := List(c.ZetDir)
+		l, err := List(c.ZetDir, `dir_name DESC`)
 		if err != nil {
 			return fmt.Errorf("Failed to retrieve list of zettels: %v", err)
 		}
@@ -41,6 +45,30 @@ func ListCmd(args []string) error {
 		}
 	case 3: // one arg
 		switch strings.ToLower(args[2]) {
+		case `recent`:
+			l, err := List(c.ZetDir, `mtime ASC`)
+			if err != nil {
+				return fmt.Errorf("Failed to retrieve list of zettels: %v", err)
+			}
+			for _, z := range l {
+				fmt.Println(z)
+			}
+		case `alpha`:
+			l, err := List(c.ZetDir, `title ASC`)
+			if err != nil {
+				return fmt.Errorf("Failed to retrieve list of zettels: %v", err)
+			}
+			for _, z := range l {
+				fmt.Println(z)
+			}
+		case `length`:
+			l, err := List(c.ZetDir, `LENGTH(body) ASC`)
+			if err != nil {
+				return fmt.Errorf("Failed to retrieve list of zettels: %v", err)
+			}
+			for _, z := range l {
+				fmt.Println(z)
+			}
 		case `help`:
 			fmt.Println(listUsage)
 			return nil
@@ -55,17 +83,17 @@ func ListCmd(args []string) error {
 
 // List retrieves a list of zettels. It synchronizes the database and
 // gets list of zettels.
-func List(zetPath string) ([]string, error) {
+func List(zetPath, sort string) ([]string, error) {
 	var l []string
 	s, err := storage.UpdateDB(zetPath)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to sync database: %v", err)
 	}
 	defer s.Close()
-	files, err := s.AllZettels()
+	files, err := s.AllZettels(sort)
 
 	for _, f := range files {
-		l = append(l, f.DirName+" "+f.Name)
+		l = append(l, f.DirName+" "+f.Title)
 	}
 	return l, nil
 }
