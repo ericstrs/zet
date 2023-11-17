7fd61e7218570eb5b1f93c06247694439bc02975
oneseIf <eric.garcia12317@gmail.com>
2023-11-17 16:12:05 -0600

    feat(content): implement links command

--- internal/meta/links.go
+++ internal/meta/links.go
@@ -1,16 +1,19 @@
 package meta
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/iuiq/zet/internal/config"
 )
 
-var linkUsage = `NAME
+const (
+	linkUsage = `NAME
 	link - prints zettel link
 
 USAGE:
@@ -19,9 +22,10 @@ USAGE:
 	zet link [isosec] - Prints zettel link for the given dir isosec.
 	zet link help     - Provides command information.`
 
-// linkFormat is the format for a zettel link. It should take the form
-// `* [dir](../dir/) title`
-var linkFormat = "* [%s](../%s) %s"
+	// linkFormat is the format for a zettel link. It should take the form
+	// `* [dir](../dir/) title`
+	linkFormat = "* [%s](../%s) %s"
+)
 
 // LinkCmd parses and validates user arguments for the link command.
 // If arguments are valid, it calls the desired operation.
@@ -117,4 +121,52 @@ func zettelDir(path string) (string, error) {
 	}
 	name := filepath.Base(path)
 	return name, nil
+}
+
+// Links returns links from a zettel at the given path.
+func Links(path string) (string, error) {
+	// This essentially locks support to just readme files.
+	if !strings.HasSuffix(path, `README.md`) {
+		path = filepath.Join(path, `README.md`)
+	}
+
+	// Does the file exist?
+	ok, err := IsFile(path)
+	if err != nil {
+		if err == ErrPathDoesNotExist {
+			return "", err
+		}
+		return "", fmt.Errorf("Failed to ensure file exists: %v", err)
+	}
+	if !ok {
+		return "", errors.New("path corresponds to a directory")
+	}
+
+	contentBytes, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	content := string(contentBytes)
+	linkLines := ParseLinks(content)
+
+	return strings.Join(linkLines, "\n"), nil
+}
+
+// ParseLinks parses out and returns the links from zettel content.
+// A link is takes the form of a line containing the substring
+// "[dir](../dir) title".
+func ParseLinks(content string) []string {
+	var linkLines []string
+	// Match lines that contain a link. E.g., `* [dir][../dir] title`
+	linkRegex := regexp.MustCompile(`\[(.+)\]\(\.\./(.*?)/?\) (.+)`)
+	scanner := bufio.NewScanner(strings.NewReader(content))
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := linkRegex.FindStringSubmatch(line)
+		if len(matches) > 0 {
+			linkLines = append(linkLines, line)
+			continue
+		}
+	}
+	return linkLines
 }
